228b156a7f13e6d1adedaec4c274d1783cb46c81
shubham agrawal <manovagyanik1@gmail.com>
2017-07-10 21:30:33 +0530

    adding comment.json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var request = require('request');
+var comments = require('./json/comments.json');
 var comment = require('./json/comment.json');
 var feed = require('./json/feed.json');
 var userReactionPost = require('./json/userReactionPost.json');
@@ -33,7 +34,7 @@ app.get('/v1/comments', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   // var userId = req.param.userId;
 
-  res.send(JSON.stringify(comment));
+  res.send(JSON.stringify(comments));
 });
 
 app.get('/v1/feed', function(req, res, next) {
@@ -46,6 +47,11 @@ app.get('/v1/feed', function(req, res, next) {
 app.post('/v1/user-reaction', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(userReactionPost));
+});
+
+app.post('/v1/comment', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(comment));
 });
 
 app.delete('/v1/user-reaction', function(req, res, next) {
